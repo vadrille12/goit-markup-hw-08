fa8d382950cb44f951b0f8e4f73c0453277ab9df
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-02-15 17:14:38 +0200

    upgrade modal

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,33 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  backdrop: document.querySelector(".js-backdrop"),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener("click", openModal);
+refs.closeModalBtn.addEventListener("click", closeModal);
+refs.backdrop.addEventListener("click", onBackdropClick);
 
-  function toggleModal() {
-    document.body.classList.toggle("modal-open")
-    refs.modal.classList.toggle('is-hidden');
+function openModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  document.body.classList.add("modal-open");
+  refs.backdrop.classList.remove("is-hidden");
+}
+
+function closeModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  document.body.classList.remove("modal-open");
+  refs.backdrop.classList.add("is-hidden");
+}
+
+function onBackdropClick(e) {
+  if (e.target === e.currentTarget) {
+    closeModal();
   }
-})();
+}
+
+function onEscKeyPress(e) {
+  if (e.code === "Escape") {
+    closeModal();
+  }
+}
